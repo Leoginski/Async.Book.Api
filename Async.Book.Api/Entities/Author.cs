ed6a5ff4450db982d35af512ae6c0ca501c651d3
Leonardo Smoginski <leonardo.smoginski@gmail.com>
2019-03-27 20:53:09 -0300

    Add project files.

---
+++ Async.Book.Api/Entities/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Async.Book.Api.Entities
+{
+    public class Author
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string LastName { get; set; }
+    }
+}
